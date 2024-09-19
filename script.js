ef69fcdece40f0315580c34e0f18f0f13c57d689
issa010101 <157200337+issa010101@users.noreply.github.com>
2024-09-19 21:20:38 +0900

    taille et designe

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // Code JavaScript facultatif pour les interactions dynamiques
 console.log('Script JavaScript chargé');
+    // Affiche le bouton lorsqu'on défile de 20px vers le bas
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        const backToTopButton = document.getElementById("backToTop");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    }
+
+    // Lorsque l'utilisateur clique sur le bouton, il remonte en haut de la page
+    document.getElementById("backToTop").onclick = function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
